1d275cbbe21b37035d474294a4a9bdf643930d7e
JosephIorio <71645078+JosephIorio@users.noreply.github.com>
2021-04-04 22:06:36 -0400

    2
    QDW

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,19 @@
+DOMSelectors = {
+    displayContainer: document.querySelector(.container)
+  };
+console.log(DomSelectors.displayContainer)
+displayContainer: document.querySelector;
 console.log("connected");
+const quiz = [
+  {
+    question: "Who Was the 8th President?",
+    options: {
+      a: "Andrew Jackson",
+      b: "Martin Van Buren",
+      c: "Henry Ford",
+    },
+    realAnswer: "b",
+    img:
+      "https://images.unsplash.com/photo-1585076799833-08afc3d50d74?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=694&q=80",
+  },
+];
