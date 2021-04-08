0bcf1a83b1a323986fc32362197c713c215da5b2
JosephIorio <joseph.iorio21@sitechhs.com>
2021-04-08 08:54:42 -0400

    eq

--- src/js/index.js
+++ src/js/index.js
@@ -2,17 +2,55 @@ DOMSelectors = {
   displayContainer: document.querySelector(".container"),
 };
 console.log("connected");
-{
-  question: "Who Was the 8th President?;",
-    {
-      options: {
-        a: "Andrew Jackson",
-        b: "Martin Van Buren",
-        c: "Henry Ford",
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
-        realAnswer: "b",
-        img:
-          "https://images.unsplash.com/photo-1585076799833-08afc3d50d74?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=694&q=80",
+  function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+    function showQuestions(questions, quizContainer){
+      var output = [];
+	var answers;
+  for(var i=0; i<questions.length; i++){
+    answers = [];
+    for(letter in questions[i].answers){
+    }
+    userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+    if(userAnswer===questions[i].correctAnswer){
+      numCorrect++;
+      answerContainers[i].style.color = 'blue';
+    }
+  
+    function showResults(questions, quizContainer, resultsContainer){
+      // code will go here
+    }
+  
+    // show the questions
+    showQuestions(questions, quizContainer);
+  
+    // when user clicks submit, show results
+    submitButton.onclick = function(){
+      showResults(questions, quizContainer, resultsContainer);
+    }
+  }
+	}
+
+{
+  var myQuestions = [
+    {
+      question: "Who Was The 8th President",
+      answers: {
+        a: 'Andrew Jackson',
+        b: 'Martin van Buren',
+        c: 'Henry Ford'
       },
-    };
-}
+      correctAnswer: 'b'
+    },
+    {
+      question: "Who was known as The Great Compromiser?",
+      answers: {
+        a: 'Richard Wheeler',
+        b: 'Ricky Jenkins',
+        c: 'Henry Clay'
+      },
+      correctAnswer: 'c'
+    }
+  ]
