e425a3d90d03a55a1c3592b35805d9e461ad27d0
JosephIorio <71645078+JosephIorio@users.noreply.github.com>
2021-04-12 15:56:49 -0400

    wew
    we

--- src/js/index.js
+++ src/js/index.js
@@ -1,67 +1,92 @@
 DOMSelectors = {
   displayContainer: document.querySelector(".container"),
 };
+const questionContainerElement = document.getElementById(".Questioncontainer");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
+
 console.log("connected");
-function generateQuiz(
-  questions,
-  quizContainer,
-  resultsContainer,
-  submitButton
-) {}
 
-function generateQuiz(
-  questions,
-  quizContainer,
-  resultsContainer,
-  submitButton
-) {}
+let shuffledQuestions, currentQuestionIndex;
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
-function showQuestions(questions, quizContainer) {
-  const output = [];
-  const answers = document.querySelectorAll;
+console.log("connected");
+
+function startGame() {
+  console.log("lets go");
+  startButton.classList.add("hide");
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove("hide");
+  setNextQuestion();
 }
-for (var i = 0; i < questions.length; i++) {
-  answers = [];
-  for (letter in questions[i].answers) {
+
+function setNextQuestion() {
+  function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach((answer) => {
+      const button = document.createElement("button");
+      button.innerText = answer.text;
+      button.classList.add("btn");
+      if (answer.correct) {
+        button.dataset.correct = answer.correct;
+      }
+      button.addEventListener("click", selectAnswer);
+      answerButtonsElement.appendChild(button);
+    });
   }
-  userAnswer = (
-    answerContainers[i].querySelector(
-      "input[name=question" + i + "]:checked"
-    ) || {}
-  ).value;
-  if (userAnswer === questions[i].correctAnswer) {
-    numCorrect++;
-    answerContainers[i].style.color = "blue";
+}
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  question.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerText = answer.text;
+    button.classList.add("btn");
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
+  Array.from(answerButtonsElement.children).forEach((button) => {
+    setStatusClass(button, button.dataset.correct);
+  });
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove("hide");
+  } else {
+    startButton.innerText = "Restart";
+    startButton.classList.remove("hide");
   }
-
-  function showResults(questions, quizContainer, resultsContainer) {}
-
-  showQuestions(questions, quizContainer);
-
-  submitButton.onclick = function () {
-    showResults(questions, quizContainer, resultsContainer);
-  };
 }
 
-{
-  const myQuestions = [
-    {
-      question: "Who Was The 8th President",
-      answers: {
-        a: "Andrew Jackson",
-        b: "Martin van Buren",
-        c: "Henry Ford",
-      },
-      correctAnswer: "b",
-    },
-    {
-      question: "Who was known as The Great Compromiser?",
-      answers: {
-        a: "Richard Wheeler",
-        b: "Ricky Jenkins",
-        c: "Henry Clay",
-      },
-      correctAnswer: "c",
-    },
-  ];
+function setStatusClass(element, correct) {
+  clearStatusClass(element);
+  if (correct) {
+    element.classList.add("correct");
+  } else {
+    element.classList.add("wrong");
+  }
 }
+
+const questions = [
+  {
+    question: "Who Was the 8th President of the United States Shown Here?",
+    answers: [
+      { text: "Andrew Jackson", correct: true },
+      { text: "Martin van Buren", correct: false },
+      { text: "Henry Ford", correct: false },
+    ],
+  },
+];
