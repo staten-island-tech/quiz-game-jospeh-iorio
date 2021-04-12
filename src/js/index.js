03fdfc9a13cd2d9b8f37d57b9dbc7968d978a2c0
JosephIorio <71645078+JosephIorio@users.noreply.github.com>
2021-04-12 18:09:31 -0400

    efwwefwe
    wfewfw

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,53 @@
-DOMSelectors = {
-  displayContainer: document.querySelector(".container"),
-};
-const questionContainerElement = document.getElementById(".Questioncontainer");
-const questionElement = document.getElementById("question");
-const answerButtonsElement = document.getElementById("answer-buttons");
 const startButton = document.getElementById("start-btn");
 const nextButton = document.getElementById("next-btn");
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+const resultForm = document.getElementById("form-result");
 
-console.log("connected");
-
+let countRightAnswers = 0;
 let shuffledQuestions, currentQuestionIndex;
+let currentQuestion = 1;
+
 startButton.addEventListener("click", startGame);
+
 nextButton.addEventListener("click", () => {
+  document.getElementById("answer-buttons").classList.remove("no-click");
+
   currentQuestionIndex++;
   setNextQuestion();
+
+  currentQuestion++;
+  document.getElementById("current-question").innerHTML = currentQuestion;
 });
 
-console.log("connected");
-
 function startGame() {
-  console.log("lets go");
+  console.log("started");
+
+  document.getElementById("answer-buttons").classList.remove("no-click");
+
   startButton.classList.add("hide");
+  resultForm.classList.add("hide");
+
   shuffledQuestions = questions.sort(() => Math.random() - 0.5);
   currentQuestionIndex = 0;
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
+
+  currentQuestion = 1;
+  document.getElementById("current-question").innerHTML = currentQuestion;
+
+  countRightAnswers = 0;
+
+  document.getElementById("all-questions2").innerHTML = questions.length;
+  document.getElementById("all-questions").innerHTML = questions.length;
 }
 
 function setNextQuestion() {
-  function showQuestion(question) {
-    questionElement.innerText = question.question;
-    question.answers.forEach((answer) => {
-      const button = document.createElement("button");
-      button.innerText = answer.text;
-      button.classList.add("btn");
-      if (answer.correct) {
-        button.dataset.correct = answer.correct;
-      }
-      button.addEventListener("click", selectAnswer);
-      answerButtonsElement.appendChild(button);
-    });
-  }
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
+
 function showQuestion(question) {
   questionElement.innerText = question.question;
   question.answers.forEach((answer) => {
@@ -56,19 +62,41 @@ function showQuestion(question) {
   });
 }
 
+function resetState() {
+  nextButton.classList.add("hide");
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+  }
+}
+
 function selectAnswer(e) {
   const selectedButton = e.target;
   const correct = selectedButton.dataset.correct;
-  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach((button) => {
     setStatusClass(button, button.dataset.correct);
   });
+
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove("hide");
   } else {
-    startButton.innerText = "Restart";
+    resultForm.classList.remove("hide");
+    questionContainerElement.classList.add("hide");
+
+    startButton.innerText = "Repeat";
     startButton.classList.remove("hide");
   }
+
+  if ((selectedButton.dataset = correct)) {
+    countRightAnswers++; //+1
+  }
+
+  document.getElementById("right-answers").innerHTML = countRightAnswers;
+  document.getElementById("answers-percent").innerHTML = (
+    (100 * countRightAnswers) /
+    questions.length
+  ).toFixed(0);
+
+  document.getElementById("answer-buttons").classList.add("no-click");
 }
 
 function setStatusClass(element, correct) {
@@ -80,13 +108,54 @@ function setStatusClass(element, correct) {
   }
 }
 
+function clearStatusClass(element) {
+  element.classList.remove("correct");
+  element.classList.remove("wrong");
+}
+
 const questions = [
   {
-    question: "Who Was the 8th President of the United States Shown Here?",
+    question: "What is the best animal",
     answers: [
-      { text: "Andrew Jackson", correct: true },
-      { text: "Martin van Buren", correct: false },
-      { text: "Henry Ford", correct: false },
+      { text: "Monkey", correct: true },
+      { text: "Gorilla", correct: false },
+      { text: "Chimp", correct: false },
+      { text: "Lemur", correct: false },
+    ],
+  },
+  {
+    question: "Am I very tired right now",
+    answers: [
+      { text: "You bet", correct: true },
+      { text: "No", correct: false },
+      { text: "HAHAHAAH", correct: false },
+      { text: "Okay", correct: false },
+    ],
+  },
+  {
+    question: "Did I spend a lot of time on this project ",
+    answers: [
+      { text: "Kinda", correct: true },
+      { text: "So much! ", correct: false },
+      { text: "Swag", correct: false },
+      { text: "Okay", correct: false },
+    ],
+  },
+  {
+    question: "Did I give up many times",
+    answers: [
+      { text: "No", correct: false },
+      { text: "Yes", correct: true },
+      { text: "Only twice", correct: false },
+      { text: "Only three times", correct: false },
+    ],
+    question:
+      "What is 8+7 due to the fact I'm running out of questions to think of ",
+    answers: [
+      { text: "15", correct: true },
+      { text: "12", correct: false },
+      { text: "18", correct: false },
+      { text: "17", correct: false },
     ],
   },
 ];
